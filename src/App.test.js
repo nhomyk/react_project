cff487f7aabb09d54dab768623a7fcff63a3c243
“nicholashomyk” <nickhomyk@gmail.com>
2024-04-04 11:58:12 -0400

    3 -

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders link', () => {
   render(<App />);
   const linkElement = screen.getByText(/CHATBOT/i);
   expect(linkElement).toBeInTheDocument();
